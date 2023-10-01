8301846c40042924c7f97f7145487dbce864340f
Podrochimaru <kostya.arn@gmail.com>
2023-10-01 16:21:33 +0300

    Branch created

---
+++ DetectiveInTime/Assets/_Source/UI_system/UI.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace UI_system
+{
+    public class UI : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
