5c440f341bbbe5f3e72eb0e23b8c4dd188cdbe01
praprf <fil18mot@gmail.com>
2023-10-02 11:39:34 +0300

    finished closet puzzle

---
+++ DetectiveInTime/Assets/_Source/PuzzleClosetSystem/Closet.cs
@@ -0,0 +1,31 @@
+using System;
+using UnityEngine;
+
+namespace PuzzleClosetSystem
+{
+    public class Closet : MonoBehaviour
+    {
+        [SerializeField] private int quantityOfClicks;
+        private Action<int> _onClosetClick;
+
+        private void Start()
+        {
+            this._onClosetClick += OnClickAdd;
+        }
+
+        void OnClickAdd(int i) => OpenTheDoor();
+        private void OnMouseDown()
+        {
+            --quantityOfClicks;
+            _onClosetClick?.Invoke(quantityOfClicks);
+        }
+
+        private void OpenTheDoor()
+        {
+            if (quantityOfClicks <= 0)
+            {
+                Debug.Log("u won");
+            }
+        }
+    }
+}
